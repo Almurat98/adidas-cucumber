a64f8db84d1ddabc9b0ec3f2e0cc150f25e36d11
Almurat98 <almurat908@gmail.com>
2021-07-13 11:07:28 -0500

    finished pages

--- src/test/java/adidas/pages/CartPage.java
+++ src/test/java/adidas/pages/CartPage.java
@@ -1,4 +1,44 @@
 package adidas.pages;
 
+import adidas.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CartPage {
+    public CartPage() {
+        PageFactory.initElements(Driver.getDriver(),this);}
+
+        @FindBy (linkText = "Delete")
+        WebElement delete;
+
+    @FindBy (xpath = "//button[@class='btn btn-success']")
+    WebElement placeOrderButton;
+
+    @FindBy(id="name")
+    WebElement inputName;
+
+    @FindBy(id="country")
+    WebElement inputCountry;
+
+    @FindBy(id="city")
+    WebElement inputCity;
+
+    @FindBy(id="card")
+    WebElement inputCreditCard;
+
+    @FindBy(id="month")
+    WebElement inputMonth;
+
+    @FindBy(id="year")
+    WebElement inputYear;
+
+    @FindBy (xpath = "//button[.='Purchase']")
+    WebElement purchaseButton;
+
+    @FindBy(xpath = "//p[@class='lead text-muted ']")
+    WebElement orderInfo;
+
+    @FindBy(xpath = "//button[@class='confirm btn btn-lg btn-primary']")
+    WebElement OKButton;
 }
--- src/test/java/adidas/pages/LaptopPage.java
+++ src/test/java/adidas/pages/LaptopPage.java
@@ -1,4 +1,20 @@
 package adidas.pages;
 
+import adidas.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LaptopPage {
+    public LaptopPage() {
+        PageFactory.initElements(Driver.getDriver(),this);}
+
+    @FindBy (linkText = "Sony vaio i5")
+    WebElement SonyVaioI5;
+
+    @FindBy(linkText = "Dell i7 8gb")
+    WebElement DellI78gb;
+
+
+
 }
