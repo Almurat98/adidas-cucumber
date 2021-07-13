c1ab1aea08a3f6652c6f27d53fcf8dbe345398d2
Almurat98 <almurat908@gmail.com>
2021-07-13 10:35:34 -0500

    build home page

--- src/test/java/adidas/pages/HomePage.java
+++ src/test/java/adidas/pages/HomePage.java
@@ -1,4 +1,20 @@
 package adidas.pages;
 
+import adidas.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+    public HomePage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (id="cartur")
+    WebElement cartButton;
+
+    @FindBy(linkText ="Laptops")
+    WebElement Laptops;
+
+
 }
